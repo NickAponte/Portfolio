a2854a7300683bb434432dcf04a0b82b4976f9d0
NickAponte <apontenick@aol.com>
2021-05-17 13:26:25 -0400

    Mvpish

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,19 +10,25 @@ function Home() {
 			articleRef.current.scrollIntoView({ behavior: 'smooth' });
 		}
 	return (
-		<div>
-			<nav className='home-nav'></nav>
-			<main className='home-main'>
-				<div className='home-logo'>
-					<h2>Welcome</h2>
-					<NavBar/>
-				</div>
-				<div className='home-header'>
-					
-				</div>
-				
-			</main>
-			<Article ref={articleRef} onBackClick={handleBackClick} />
+		// <div>
+		// 	<nav className='home-nav'></nav>
+		// 	<main className='home-main'>
+		// 		<div className='home-logo'>
+		// 			<h2>Welcome</h2>
+		// 			<NavBar/>
+		// 		</div>
+		// 		<div className='home-header'>
+
+		// 		</div>
+
+		// 	</main>
+		// 	<Article ref={articleRef} onBackClick={handleBackClick} />
+		// </div>
+		<div className='welcome'>
+			<Link to='/article' className='enterBtn'>
+				Welcome to my Portfolio, Click here!
+			</Link>
+			<img className='bulb' src='images/bulb_cropped.png' alt='bulb' />
 		</div>
 	);
 }
